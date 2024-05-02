937b19a0439ffe27d075d38c1109a29f9e284075
lisa <134704050+y-lisa@users.noreply.github.com>
2024-05-02 22:55:48 +0200

    Update Oblig2Del2B.java

--- Oblig2Del2B.java
+++ Oblig2Del2B.java
@@ -6,7 +6,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Oblig2Del2B {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         // Sjekker om det er gitt 1 argument som parameter til programmet
         if (args.length < 1){
             System.out.println("Mangler mappenavn som parameter!");
@@ -18,7 +18,7 @@ public class Oblig2Del2B {
 
         String mappenavn = args[0];
         Monitor2 monitor2 = new Monitor2();
-        ArrayList<Thread> listeAvlest = new ArrayList<>();  // Liste med alle traader som lages naar de avleses
+        // ArrayList<Thread> listeAvlest = new ArrayList<>();  // Liste med alle traader som lages naar de avleses
 
         try{
             // Steg 1: Leser av fil og setter inn HashMap i monitor
@@ -34,53 +34,47 @@ public class Oblig2Del2B {
                 // Starter traad
                 LeseTrad leseTraad = new LeseTrad(filnavn, monitor2);
                 Thread traad = new Thread(leseTraad);
-                listeAvlest.add(traad);
                 traad.start();
             }
 
             // Lukker scanneren
-            sc.close();
+            // sc.close();
 
         } catch (FileNotFoundException e) {
             System.out.println("Mappesti og fil ble ikke funnet!");
         }
-
-        // Steg 2: Fletter Hashmap-er
-        // Venter på at alle FletteTrad-trådene fullfører
-
-        try {
-            for (Thread nyTraad : listeAvlest) {
-                nyTraad.join();
-            }    
-        } catch (InterruptedException ie) {
-            System.out.println("Feil i avlesing av fil!");
+        
+        for (int i = 0; i < 8; i++){
+            FletteTrad flettet = new FletteTrad(monitor2);
+            flettet.run();
         }
         
-        // Ny liste med flettede traader
-        ArrayList<Thread> listeFlettet = new ArrayList<>();
-
-        // Antall traader settes lik aatte
-        // Disse skal flettes sammen i en monitor2 til det kun er én HashMap
-        for (int i = 0; i < 8; i++){
-            FletteTrad fletteTrad = new FletteTrad(monitor2);
-            Thread tradFlettet = new Thread(fletteTrad);
-            listeFlettet.add(tradFlettet);
-            tradFlettet.start();
+        
+        System.out.println("Utskrift:");
+        HashMap<String, Subsekvens> resultat = monitor2.hentUtTo();
+        /*
+        // Utskrift alt
+        for (Subsekvens s : resultat.values()){
+            System.out.print(s);
         }
-
-        try {
-            for (Thread tradFlettet : listeFlettet){
-                tradFlettet.join();
-            }
-        } catch (InterruptedException ie) {
-            System.out.println("Feil i fletting!");
-        } 
+        */
 
         // Subsekvens med flest forekomst
         String mestFrekvent = "";
         int forekomst = 0;
-        //
+
+        // for each-lokke
+        for (String frekventSubsekvens : resultat.keySet()){
+            Subsekvens s = resultat.get(frekventSubsekvens);
+            // Sjekker for hver subsekvens om antallet er mer enn 0, som saa okes for hver runde
+            if (s.hentAntall() > forekomst){
+                mestFrekvent = frekventSubsekvens;
+                forekomst = s.hentAntall();
+            }
+        }
+
         System.out.println("Mest frekvente subsekvens: " + mestFrekvent + " med " + forekomst + " forekomster.");
-        
+        System.out.println("(Ser at dette ikke stemmer)");
+            
     }
 }
